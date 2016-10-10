b72919cf5a88d3fb648fb94b0e4dfc35af440d52
WangZi <yhjhoo@gmail.com>
2016-10-10 17:44:20 +0800

    init

---
+++ src/main/java/me/prince/JpaBootApplication.java
@@ -0,0 +1,28 @@
+package me.prince;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBootApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(PersonRepository repo){
+		return (args) -> {
+			repo.save(new Person("f1", "l2"));
+			
+			repo.save(new Person("f1", "l2"));
+			
+			repo.save(new Person("f1", "l2"));
+			
+			repo.save(new Person("f1", "l2"));
+		};
+		
+	}
+}
---
+++ src/main/java/me/prince/PersonController.java
@@ -0,0 +1,20 @@
+package me.prince;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/person")
+public class PersonController {
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public  Iterable<Person> getAll(){
+		return personRepository.findAll();
+	}
+}
